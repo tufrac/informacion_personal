2fdf3d64649f60d4f24241cb8b14f6d2b04f87cb
Francisco <tufrac@gmail.com>
2017-06-18 14:23:22 -0600

    first commit

---
+++ app/src/main/java/com/wibej/informacionpersonal/MainActivity.java
@@ -0,0 +1,30 @@
+package com.wibej.informacionpersonal;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSiguiente = (Button) findViewById(R.id.btnSiguiente);
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ConfirmationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
---
+++ app/src/main/java/com/wibej/informacionpersonal/PersonaInformation.java
@@ -0,0 +1,61 @@
+package com.wibej.informacionpersonal;
+
+/**
+ * Created by tufrac on 6/18/17.
+ */
+
+public class PersonaInformation {
+    private String nombre;
+    private String fechaNacimiento;
+    private String telefono;
+    private String email;
+    private String descripcion;
+
+    public PersonaInformation(String nombre, String fechaNacimiento, String telefono, String email, String descripcion) {
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.telefono = telefono;
+        this.email = email;
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
