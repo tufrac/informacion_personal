786c95f5cbd5f12e8fdedae569700cf8f0cb6839
Francisco <tufrac@gmail.com>
2017-06-19 11:21:00 -0600

    add screenshots

--- app/src/main/java/com/wibej/informacionpersonal/ConfirmationActivity.java
+++ app/src/main/java/com/wibej/informacionpersonal/ConfirmationActivity.java
@@ -1,7 +1,10 @@
 package com.wibej.informacionpersonal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ConfirmationActivity extends AppCompatActivity {
 
@@ -9,5 +12,43 @@ public class ConfirmationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation);
+
+        Bundle data = getIntent().getExtras();
+        String nombre = data.getString(getResources().getString(R.string.info_name));
+        String fechaNacimiento = data.getString(getResources().getString(R.string.info_fecha_nacimiento));
+        String telefono = data.getString(getResources().getString(R.string.info_telefono));
+        String email = data.getString(getResources().getString(R.string.info_email));
+        String descripcion = data.getString(getResources().getString(R.string.info_descripcion));
+
+        TextView tvNombre = (TextView) findViewById(R.id.tvName);
+        TextView tvFechaNacimiento = (TextView) findViewById(R.id.tvInfoFechaNacimiento);
+        TextView tvTelefono = (TextView) findViewById(R.id.tvInfoTelefono);
+        TextView tvEmail = (TextView) findViewById(R.id.tvInfoEmail);
+        TextView tvDescripcion = (TextView) findViewById(R.id.tvInfoDescripcion);
+
+        tvNombre.setText(nombre);
+        tvFechaNacimiento.setText(fechaNacimiento);
+        tvEmail.setText(email);
+        tvTelefono.setText(telefono);
+        tvDescripcion.setText(descripcion);
+    }
+
+    public void backEdit(View v){
+        Intent back = new Intent(this, MainActivity.class);
+
+        Bundle data = getIntent().getExtras();
+        String nombre = data.getString(getResources().getString(R.string.info_name));
+        String fechaNacimiento = data.getString(getResources().getString(R.string.info_fecha_nacimiento));
+        String telefono = data.getString(getResources().getString(R.string.info_telefono));
+        String email = data.getString(getResources().getString(R.string.info_email));
+        String descripcion = data.getString(getResources().getString(R.string.info_descripcion));
+
+        back.putExtra(getResources().getString(R.string.info_name), nombre);
+        back.putExtra(getResources().getString(R.string.info_fecha_nacimiento), fechaNacimiento);
+        back.putExtra(getResources().getString(R.string.info_telefono), telefono);
+        back.putExtra(getResources().getString(R.string.info_email), email);
+        back.putExtra(getResources().getString(R.string.info_descripcion), descripcion);
+
+        startActivity(back);
     }
 }
--- app/src/main/java/com/wibej/informacionpersonal/MainActivity.java
+++ app/src/main/java/com/wibej/informacionpersonal/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
@@ -16,15 +18,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnSiguiente = (Button) findViewById(R.id.btnSiguiente);
-        btnSiguiente.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ConfirmationActivity.class);
-                startActivity(intent);
-            }
-        });
+        Bundle data = getIntent().getExtras();
+        if(data != null){
+            String personName = data.getString(getResources().getString(R.string.info_name));
+            String fechaNacimiento = data.getString(getResources().getString(R.string.info_fecha_nacimiento));
+            String telefono = data.getString(getResources().getString(R.string.info_telefono));
+            String email = data.getString(getResources().getString(R.string.info_email));
+            String descripcion = data.getString(getResources().getString(R.string.info_descripcion));
 
+            EditText etPersonName = (EditText) findViewById(R.id.etNombreCompleto);
+            DatePicker etFechaNacimiento = (DatePicker) findViewById(R.id.dpFechaNacimiento);
+            EditText etTelefono = (EditText) findViewById(R.id.etTelefono);
+            EditText etEmail = (EditText) findViewById(R.id.etEmail);
+            EditText etDescripcion = (EditText) findViewById(R.id.etDescripcion );
+
+            etPersonName.setText(personName);
+
+            etTelefono.setText(telefono);
+            etEmail.setText(email);
+            etDescripcion.setText(descripcion);
+        }
     }
 
+    public void loadConfirmation(View v){
+        Intent siguiente = new Intent(this, ConfirmationActivity.class);
+        EditText personName = (EditText) findViewById(R.id.etNombreCompleto);
+        DatePicker fechaNacimiento = (DatePicker) findViewById(R.id.dpFechaNacimiento);
+        EditText telefono = (EditText) findViewById(R.id.etTelefono);
+        EditText email = (EditText) findViewById(R.id.etEmail);
+        EditText descripcion = (EditText) findViewById(R.id.etDescripcion );
+        int currentDate[] = new int[3];
+        currentDate[0] = fechaNacimiento.getDayOfMonth();
+        currentDate[1] = fechaNacimiento.getMonth();
+        currentDate[2] = fechaNacimiento.getYear();
+
+
+        siguiente.putExtra(getResources().getString(R.string.info_name), personName.getText().toString());
+        siguiente.putExtra(getResources().getString(R.string.info_fecha_nacimiento), currentDate);
+        siguiente.putExtra(getResources().getString(R.string.info_telefono), telefono.getText().toString());
+        siguiente.putExtra(getResources().getString(R.string.info_email), email.getText().toString());
+        siguiente.putExtra(getResources().getString(R.string.info_descripcion), descripcion.getText().toString());
+        startActivity(siguiente);
+    }
 }
